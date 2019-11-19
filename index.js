9fc73f4b21835bf3d57754d67455fec352d10ab2
Craig Donavin <crdonavin@gmail.com>
2019-11-19 21:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Code your solution here
 function findMatching(drivers, string){
-  let arr = drivers.filter(el =>{
+  return drivers.filter(el =>{
      el.toLowerCase() === string.toLowerCase()
   })
-  return arr
 }
 
 function fuzzyMatch() {
