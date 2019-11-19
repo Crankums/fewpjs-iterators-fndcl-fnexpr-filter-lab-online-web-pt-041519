4b9509f837475b919ff17267f1ea5b35cb137b4b
Craig Donavin <crdonavin@gmail.com>
2019-11-19 22:04:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, string) {
-  return drivers.filter(key => 
+  return drivers.filter(key =>
   key.name.toLowerCase === string.toLowerCase)
 }
